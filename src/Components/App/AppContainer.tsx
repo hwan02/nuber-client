772176ba4980427b2226d056a30cd9feb10aeff4
Daniel <ssh32009757@gmail.com>
2020-05-05 15:57:18 +0900

    20200505

---
+++ src/Components/App/AppContainer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { graphql } from "react-apollo";
+import AppPresenter from "./AppPresenter";
+import { IS_LOGGED_IN } from "./AppQueries";
+import { ThemeProvider } from "../../typed-components";
+import theme from '../../theme';
+import GlobalStyle from "../../global-styles";
+
+
+const AppContainer = ({data}:{data?:any}) => 
+(
+    <ThemeProvider theme ={theme}>
+        <React.Fragment>
+            <GlobalStyle/>
+            <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+        </React.Fragment>
+    </ThemeProvider>
+);
+
+export default graphql(IS_LOGGED_IN)(AppContainer);
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import ReactDOM from "react-dom";
+import client from "./apollo";
+import App from "./Components/App";
+import "./global-styles";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement);
